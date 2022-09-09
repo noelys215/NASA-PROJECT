4332610662f8a7ddf3644b0209b1e6f6b1cddad7
Henry B <noelys215@gmail.com>
2022-09-09 19:28:18 -0400

    added tests

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,70 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Test GET /launches', () => {
+	test('Should respond with 200', async () => {
+		const response = await request(app)
+			.get('/launches')
+			.expect('Content-Type', /json/)
+			.expect(200);
+	});
+});
+
+describe('Test POST /launch', () => {
+	const completeLaunchData = {
+		mission: 'NERV',
+		rocket: 'TEST-TYPE-01',
+		target: 'Kepler-186f',
+		launchDate: 'January 4, 2028',
+	};
+	const launchDataWithoutDate = {
+		mission: 'NERV',
+		rocket: 'TEST-TYPE-01',
+		target: 'Kepler-186f',
+	};
+	const launchDataWithInvalidDate = {
+		mission: 'NERV',
+		rocket: 'TEST-TYPE-01',
+		target: 'Kepler-186f',
+		launchDate: 'baka',
+	};
+
+	test('Response with 201 created', async () => {
+		const response = await request(app)
+			.post('/launches')
+			.send(completeLaunchData)
+			.expect('Content-Type', /json/)
+			.expect(201);
+
+		const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+		const responseDate = new Date(response.body.launchDate).valueOf();
+
+		expect(responseDate).toBe(requestDate);
+
+		expect(response.body).toMatchObject(launchDataWithoutDate);
+	});
+
+	test('Should catch missing required properties', async () => {
+		const response = await request(app)
+			.post('/launches')
+			.send(launchDataWithoutDate)
+			.expect('Content-Type', /json/)
+			.expect(400);
+
+		expect(response.body).toStrictEqual({
+			error: 'Missing required launch properties',
+		});
+	});
+
+	test('Should catch invalid dates', async () => {
+		const response = await request(app)
+			.post('/launches')
+			.send(launchDataWithInvalidDate)
+			.expect('Content-Type', /json/)
+			.expect(400);
+
+		expect(response.body).toStrictEqual({
+			error: 'Invalid launch date',
+		});
+	});
+});
