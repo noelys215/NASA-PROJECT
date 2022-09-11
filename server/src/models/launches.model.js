0f0e6fb80c29cd21b56d54cd463746763e00b915
Henry B <noelys215@gmail.com>
2022-09-11 13:43:43 -0400

    added mongoose database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,51 +1,90 @@
-const launches = new Map();
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
 	flightNumber: 100,
 	mission: 'NERV Exploration X',
 	rocket: 'AAA Wunder',
-	launchDate: new Date('December 25th, 2030'),
+	launchDate: new Date('December 25, 2030'),
 	target: 'Kepler-442b',
 	customers: ['NERV', 'NASA'],
 	upcoming: true,
 	success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-	return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+	return await launchesDatabase.findOne({
+		flightNumber: launchId,
+	});
 }
 
-function getAllLaunches() {
-	return Array.from(launches.values());
+async function getLatestFlightNumber() {
+	const latestLaunch = await launchesDatabase.findOne().sort('-flightNumber');
+
+	if (!latestLaunch) {
+		return DEFAULT_FLIGHT_NUMBER;
+	}
+
+	return latestLaunch.flightNumber;
 }
 
-function addNewLaunch(launch) {
-	latestFlightNumber++;
-	launches.set(
-		latestFlightNumber,
-		Object.assign(launch, {
-			success: true,
-			upcoming: true,
-			customers: ['NERV', 'NASA'],
-			flightNumber: latestFlightNumber,
-		})
+async function getAllLaunches() {
+	return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+	await launchesDatabase.findOneAndUpdate(
+		{
+			flightNumber: launch.flightNumber,
+		},
+		launch,
+		{
+			upsert: true,
+		}
 	);
 }
 
-function abortLaunchById(launchId) {
-	const aborted = launches.get(launchId);
-	aborted.upcoming = false;
-	aborted.success = false;
-	return aborted;
+async function scheduleNewLaunch(launch) {
+	const planet = await planets.findOne({
+		keplerName: launch.target,
+	});
+
+	if (!planet) {
+		throw new Error('No matching planet found');
+	}
+
+	const newFlightNumber = (await getLatestFlightNumber()) + 1;
+	const newLaunch = Object.assign(launch, {
+		success: true,
+		upcoming: true,
+		customers: ['NERV', 'NASA'],
+		flightNumber: newFlightNumber,
+	});
+
+	await saveLaunch(newLaunch);
+}
+
+async function abortLaunchById(launchId) {
+	const aborted = await launchesDatabase.updateOne(
+		{
+			flightNumber: launchId,
+		},
+		{
+			upcoming: false,
+			success: false,
+		}
+	);
+
+	return aborted.modifiedCount === 1;
 }
 
 module.exports = {
 	existsLaunchWithId,
 	getAllLaunches,
-	addNewLaunch,
 	abortLaunchById,
+	scheduleNewLaunch,
 };
--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,70 +1,81 @@
 const request = require('supertest');
 const app = require('../../app');
+const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
 
-describe('Test GET /launches', () => {
-	test('Should respond with 200', async () => {
-		const response = await request(app)
-			.get('/launches')
-			.expect('Content-Type', /json/)
-			.expect(200);
-	});
-});
-
-describe('Test POST /launch', () => {
-	const completeLaunchData = {
-		mission: 'NERV',
-		rocket: 'TEST-TYPE-01',
-		target: 'Kepler-186f',
-		launchDate: 'January 4, 2028',
-	};
-	const launchDataWithoutDate = {
-		mission: 'NERV',
-		rocket: 'TEST-TYPE-01',
-		target: 'Kepler-186f',
-	};
-	const launchDataWithInvalidDate = {
-		mission: 'NERV',
-		rocket: 'TEST-TYPE-01',
-		target: 'Kepler-186f',
-		launchDate: 'baka',
-	};
-
-	test('Response with 201 created', async () => {
-		const response = await request(app)
-			.post('/launches')
-			.send(completeLaunchData)
-			.expect('Content-Type', /json/)
-			.expect(201);
-
-		const requestDate = new Date(completeLaunchData.launchDate).valueOf();
-		const responseDate = new Date(response.body.launchDate).valueOf();
-
-		expect(responseDate).toBe(requestDate);
-
-		expect(response.body).toMatchObject(launchDataWithoutDate);
+describe('Launches API', () => {
+	beforeAll(async () => {
+		await mongoConnect();
 	});
 
-	test('Should catch missing required properties', async () => {
-		const response = await request(app)
-			.post('/launches')
-			.send(launchDataWithoutDate)
-			.expect('Content-Type', /json/)
-			.expect(400);
+	afterAll(async () => {
+		await mongoDisconnect();
+	});
 
-		expect(response.body).toStrictEqual({
-			error: 'Missing required launch properties',
+	describe('Test GET /launches', () => {
+		test('Should respond with 200', async () => {
+			const response = await request(app)
+				.get('/launches')
+				.expect('Content-Type', /json/)
+				.expect(200);
 		});
 	});
 
-	test('Should catch invalid dates', async () => {
-		const response = await request(app)
-			.post('/launches')
-			.send(launchDataWithInvalidDate)
-			.expect('Content-Type', /json/)
-			.expect(400);
+	describe('Test POST /launch', () => {
+		const completeLaunchData = {
+			mission: 'NERV',
+			rocket: 'TEST-TYPE-01',
+			target: 'Kepler-1652 b',
+			launchDate: 'January 4, 2028',
+		};
+		const launchDataWithoutDate = {
+			mission: 'NERV',
+			rocket: 'TEST-TYPE-01',
+			target: 'Kepler-1652 b',
+		};
+		const launchDataWithInvalidDate = {
+			mission: 'NERV',
+			rocket: 'TEST-TYPE-01',
+			target: 'Kepler-1652 b',
+			launchDate: 'baka',
+		};
 
-		expect(response.body).toStrictEqual({
-			error: 'Invalid launch date',
+		test('Response with 201 created', async () => {
+			const response = await request(app)
+				.post('/launches')
+				.send(completeLaunchData)
+				.expect('Content-Type', /json/)
+				.expect(201);
+
+			const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+			const responseDate = new Date(response.body.launchDate).valueOf();
+
+			expect(responseDate).toBe(requestDate);
+
+			expect(response.body).toMatchObject(launchDataWithoutDate);
+		});
+
+		test('Should catch missing required properties', async () => {
+			const response = await request(app)
+				.post('/launches')
+				.send(launchDataWithoutDate)
+				.expect('Content-Type', /json/)
+				.expect(400);
+
+			expect(response.body).toStrictEqual({
+				error: 'Missing required launch properties',
+			});
+		});
+
+		test('Should catch invalid dates', async () => {
+			const response = await request(app)
+				.post('/launches')
+				.send(launchDataWithInvalidDate)
+				.expect('Content-Type', /json/)
+				.expect(400);
+
+			expect(response.body).toStrictEqual({
+				error: 'Invalid launch date',
+			});
 		});
 	});
 });
